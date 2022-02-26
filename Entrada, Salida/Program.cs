bbd9e96843a5069a2b77ffdab2067e2966a65895
HectorGalvez11 <99548245+HectorGalvez11@users.noreply.github.com>
2022-02-25 22:44:01 -0600

    Nuevo Archivo

---
+++ Entrada, Salida/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Entrada__Salida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Escriba tu nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine(" Hola "+ nombre );
+            Console.Read();
+        }
+    }
+}
---
+++ LeerTeclado/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeerTeclado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Suma de dos numeros ");
+            Console.Write(" Ingrese su número 1: ");
+            string dato = Console.ReadLine();
+            int n1 = int.Parse(dato);
+
+            Console.Write(" Ingrese numero 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1 + n2;
+
+
+            Console.WriteLine(" El resultado es :{0} ", + suma );
+        }
+    }
+}
